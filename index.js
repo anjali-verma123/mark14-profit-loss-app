90a6a7c6805b180c0e453b776946e8386bc73b2b
Anjali Verma <av498565@gmail.com>
2022-12-17 17:18:26 +0530

    New function
    Function Submit handler

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ var currentPrice = document.querySelector("#current-price");
 var submitBtn = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#output-box");
 
+// Step - Adding Event Listeners
+submitBtn.addEventListener('click', submitHandler);
+
+function submitHandler() {
+	var iPrice = Number(initialPrice.value);
+	var cPrice = Number(currentPrice.value);
+	var sQuantity = Number(stocksQuantity.value);
+
+	calculateProfitOrLoss(iPrice, sQuantity, cPrice);
+}
